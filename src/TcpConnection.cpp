ae660829dd890c8216e4d09c77c9ddadfc5b57e6
zhangyuke-coder <1034643857@qq.com>
2023-03-17 15:15:46 +0800

    TcpConnection成功构建

---
+++ src/TcpConnection.cpp
@@ -0,0 +1,80 @@
+#include "TcpConnection.h"
+namespace zcoco {
+
+TcpConnection::TcpConnection(Socket::Ptr socket, IpAddress peer) 
+		:conn_socket_(socket),
+		peer_addr_(peer) {
+}
+
+ssize_t TcpConnection::read(void* buf, size_t count) {
+	return conn_socket_->read(buf, count);
+}
+
+ssize_t TcpConnection::readn(void* buf, size_t count) {
+	size_t readn = 0;
+	size_t left = count;
+	while (left > 0) {
+		ssize_t n = read(static_cast<char*>(buf) + readn, left);
+		if (n <= 0) {
+			return readn;
+		}
+		readn += n;
+		left -= n;
+	}
+	return count;
+}
+
+ssize_t TcpConnection::read(Buffer::Ptr buf) {
+	return buf->readSocket(conn_socket_);
+}
+
+ssize_t TcpConnection::write(const void* buf, size_t count) {
+	return conn_socket_->write(buf, count);
+}
+
+ssize_t TcpConnection::writen(const void* buf, size_t count) {
+	size_t writen = 0;
+	size_t left = count;
+	while (left > 0) {
+		ssize_t n = write(static_cast<const char*>(buf) + writen, left);
+		if (n <= 0) {
+			return writen;
+		}
+		writen += n;
+		left -= n;
+	}
+	return count;
+}
+
+ssize_t TcpConnection::write(Buffer::Ptr buf) {
+	ssize_t n = writen(buf->peek(), buf->readableBytes());
+	if (n > 0) {
+		buf->retrieve(n);
+	}
+	return n;
+}
+
+ssize_t TcpConnection::write(const std::string& message) {
+	return writen(message.data(), message.size());
+}
+
+void TcpConnection::shutdown() {
+	conn_socket_->shutdownWrite();
+}
+
+void TcpConnection::readUntilZero() {
+	Buffer::Ptr buffer(new Buffer);
+	while (read(buffer) > 0) {
+		buffer->retrieveAll();
+	}
+	return;
+}
+
+void TcpConnection::close() {
+	conn_socket_->close();
+}
+
+void TcpConnection::setTcpNoDelay(bool on) {
+	conn_socket_->setTcpNoDelay(on);
+}
+}
---
+++ src/TcpConnection.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Address.h"
+#include "Buffer.h"
+#include "Socket.h"
+
+namespace zcoco {
+class TcpConnection {
+public:
+	typedef std::shared_ptr<TcpConnection> Ptr;
+	explicit TcpConnection(Socket::Ptr socket, IpAddress peer);
+
+	ssize_t read(void* buf, size_t count);
+	ssize_t readn(void* buf, size_t count);
+	ssize_t read(Buffer::Ptr);
+	ssize_t write(const void* buf, size_t count);
+	ssize_t writen(const void* buf, size_t count);
+	ssize_t write(Buffer::Ptr);
+	ssize_t write(const std::string& message);
+	void shutdown();
+	void readUntilZero();
+	void close();
+	void setTcpNoDelay(bool on);
+
+	const IpAddress& peerAddr() const { return peer_addr_; }
+private:
+	Socket::Ptr conn_socket_;
+	IpAddress peer_addr_;
+};   
+}
